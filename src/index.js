23578a4c92a864a9657cc675537a928021b14672
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2021-08-30 14:33:01 +0200

    lint

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 const fs = require('fs')
-const loki = require('lokijs')
+const LokiJS = require('lokijs')
 const async = require('async')
 
 const populateDatabase = require('./populateDatabase')
 
-const db = new loki('test.db')
+const db = new LokiJS('test.db')
 
 const tables = {
   audit: db.addCollection('audit'),
   production: db.addCollection('production')
 }
 
-const path = '/var/log/gitlab/gitlab-rails/'
+// const path = '/var/log/gitlab/gitlab-rails/'
+const path = '/tmp/'
 
 function init (callback) {
   fs.readdir(
     path,
     (err, files) => {
+      if (err) {
+        console.error(err)
+        process.exit(1)
+      }
+
       async.each(files, (file, done) => {
         if (file.match(/^audit_json\.log/)) {
           populateDatabase(tables.audit, path + file, done)
@@ -31,13 +37,15 @@ function init (callback) {
 }
 
 init(() => {
-  const allLoginAttempts = tables.production.find({path: {'$regex': /^\/users\/(auth\/|sign_in)/}, method: 'POST'})
+  const allLoginAttempts = tables.production.find({ path: { $regex: /^\/users\/(auth\/|sign_in)/ }, method: 'POST' })
 
   allLoginAttempts.forEach(attempt => {
-    let params = {}
-    attempt.params.forEach(param => params[param.key] = param.value)
+    const params = {}
+    attempt.params.forEach(param => {
+      params[param.key] = param.value
+    })
 
-    const login = tables.audit.find({correlation_id: attempt.correlation_id})
+    const login = tables.audit.find({ correlation_id: attempt.correlation_id })
 
     console.log(attempt.time + '\t' + attempt.remote_ip + '\t' + (params.username || params.user.login) + '\t' + (login.length ? 'success' : 'fail'))
   })
